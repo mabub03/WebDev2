f486365081b5d07a01ccdd4f21adac2f0053d7fa
mabub03 <mabub03@gmail.com>
2021-03-11 13:24:17 -0500

    commit

---
+++ Project1/futureCareers.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Future Employment</title>
+    <!--Import Google Icon Font-->
+    <link
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet"
+    />
+    <!--Import materialize.css-->
+    <link
+      type="text/css"
+      rel="stylesheet"
+      href="css/materialize.min.css"
+      media="screen,projection"
+    />
+  </head>
+  <body>
+    <table class="showData"></table>
+    <!--JavaScript at end of body for optimized loading-->
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+  </body>
+</html>
